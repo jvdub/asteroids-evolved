e93017a58167690bd56afa2848139706e049d433
usuvorb-admin <j.t.van_wag@aggiemail.usu.edu>
2014-03-15 18:12:01 -0600

    Adding an object for the player's ship. This will take the fireMissile logic out of the Graphics class and into the player's ship. This should clean up the code a little bit going forward.

---
+++ app/scripts/spaceship.js
@@ -0,0 +1,54 @@
+﻿game.spaceship = (function () {
+    var spaceship = null,
+        particles = [];
+
+    // Fires a missle from the front of the ship
+    function fireMissile() {
+        // Prevent a missile from firing if one has just been fired.
+        if (game.bulletIntervalCountdown < 0) {
+            console.log("missle fired");
+            // Reset the countdown
+            game.bulletIntervalCountdown = game.BULLET_INTERVAL;
+            // Add the missile to the objects in the game
+            game.objectsInPlay[game.objectNames++] = game.Graphics.Texture({
+                image: game.images['images/missile.png'],
+                center: { x: that.x + that.directionVector.x * 50, y: that.y + that.directionVector.y * 50 },
+                width: 40, height: 25,
+                rotation: 0,
+                moveRate: 500,         // pixels per second
+                rotateRate: Math.PI,   // Radians per second
+                startVector: { x: that.directionVector.x, y: that.directionVector.y },
+                initialRotation: spec.rotation,
+                lifetime: 3000
+            });
+        }
+    }
+
+    // Updates the ship's position and angle
+    function update(time) {
+
+    }
+
+    // Renders the ship to the canvas
+    function draw() {
+
+    }
+
+    // Create the ship and the particle systems
+    function init(ship, parts) {
+        // Create the ship image
+        spaceship = game.Graphics.Texture(ship);
+
+        // Loop through the particle systems given and create a new system for each one
+        for (var i = 0, l = parts.length; i < l; ++i) {
+            particles.push(particleSystem(parts[i], game.Graphics));
+        }
+    }
+
+    return {
+        init: init,
+        update: update,
+        draw: draw,
+        fireMissile: fireMissile
+    };
+}());
