970ab251056a1e4b5682ecf6579822f7dcc28c33
usuvorb-admin <j.t.van_wag@aggiemail.usu.edu>
2014-03-17 13:02:23 -0600

    Fixing crashing with firing and moving the ship.

--- app/scripts/spaceship.js
+++ app/scripts/spaceship.js
@@ -2,16 +2,16 @@
     var spaceship = null,
         particles = [];
 
-    function moveUp() {
-        spaceship.moveUp();
+    function moveUp(time) {
+        spaceship.moveUp(time);
     }
 
-    function rotateLeft() {
-        spaceship.rotateLeft();
+    function rotateLeft(time) {
+        spaceship.rotateLeft(time);
     }
 
-    function rotateRight() {
-        spaceship.rotateRight();
+    function rotateRight(time) {
+        spaceship.rotateRight(time);
     }
 
     // Fires a missle from the front of the ship
@@ -24,13 +24,13 @@
             // Add the missile to the objects in the game
             game.objectsInPlay[game.objectNames++] = game.Graphics.Texture({
                 image: game.images['images/missile.png'],
-                center: { x: that.x + that.directionVector.x * 50, y: that.y + that.directionVector.y * 50 },
+                center: { x: spaceship.x + spaceship.directionVector.x * 50, y: spaceship.y + spaceship.directionVector.y * 50 },
                 width: 40, height: 25,
                 rotation: 0,
                 moveRate: 500,         // pixels per second
                 rotateRate: Math.PI,   // Radians per second
-                startVector: { x: that.directionVector.x, y: that.directionVector.y },
-                initialRotation: spec.rotation,
+                startVector: { x: spaceship.directionVector.x, y: spaceship.directionVector.y },
+                initialRotation: spaceship.rotation,
                 lifetime: 3000
             });
         }
